1a6d4941c7455c087e988db9e35612e80ad80bfb
Yağmur İnan <112933979+yumin0095@users.noreply.github.com>
2023-11-09 22:42:23 +0300

    skeleton added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import { fetchProducts } from "./api/hello";
 import ProductCard from "@/components/ProductCard";
 import Navbar from "@/components/Navbar";
+import CardSkeleton from "@/components/ProductCard/CardSkeleton";
+
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const addToFavorite = (product) => {
     const currentFavorites =
       JSON.parse(localStorage.getItem("favorites")) || [];
@@ -15,30 +19,42 @@ export default function Home() {
       localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
     }
   };
+
   const getProducts = async () => {
     try {
       const allProducts = await fetchProducts();
       setProducts(allProducts);
+      setLoading(false);
     } catch (error) {
       console.error("Ürünler yüklenirken hata oluştu.");
     }
   };
+
   useEffect(() => {
     getProducts();
   }, []);
-  console.log(products);
+
   return (
     <>
       <Navbar />
       <div className="grid sm:grid-cols-4 grid-cols-2 gap-y-8">
-        {products.map((product) => (
-          <ProductCard
-            product={product}
-            action={() => {
-              addToFavorite(product);
-            }}
-          />
-        ))}
+        {loading ? (
+        
+          Array.from({ length: 20 }).map((_, index) => {
+            return <CardSkeleton key={index} />;
+          })
+        ) : (
+         
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              action={() => {
+                addToFavorite(product);
+              }}
+            />
+          ))
+        )}
       </div>
     </>
   );
